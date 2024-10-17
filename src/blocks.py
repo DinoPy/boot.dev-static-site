7d822903fe85d1ddbbfe7af43c851e67fe13c1c5
DinoPy <al3x.dinoiu@gmail.com>
2024-10-17 13:11:30 +0300

    added markdown_to_blocks, block_to_block_type and markdown_block_to_html functions + tests for each

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,72 @@
+from enum import Enum
+from re import match, sub
+from src.htmlnode import HTMLNode, LeafNode
+
+
+class MarkdownTypes(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     # takes raw markdown string and returns a list of block strings
-    splits = markdown.split("\n")
-    print(splits)
+    splits = markdown.split("\n\n")
+    splits = list(map(lambda x: x.strip(), splits))
+    return splits
+
+
+def block_to_block_type(markdown_block):
+    # returns block type
+    if markdown_block.startswith("#"):
+        return MarkdownTypes.HEADING
+    if markdown_block.startswith("```"):
+        return MarkdownTypes.CODE
+    if markdown_block.startswith(">"):
+        return MarkdownTypes.QUOTE
+    if markdown_block.startswith("* ") or markdown_block.startswith("- "):
+        return MarkdownTypes.UNORDERED_LIST
+    if match(r"^\d.", markdown_block):
+        return MarkdownTypes.ORDERED_LIST
+    return MarkdownTypes.PARAGRAPH
+
+
+def markdown_block_to_html(markdown_block):
+    match block_to_block_type(markdown_block):
+        case MarkdownTypes.PARAGRAPH:
+            return HTMLNode("p", markdown_block)
+        case MarkdownTypes.HEADING:
+            heading_level = len(match(r"^#+", markdown_block).group(0))
+            stripped_block = sub(r"^#+ ", "", markdown_block)
+            return HTMLNode(f"h{heading_level}", stripped_block)
+        case MarkdownTypes.CODE:
+            stripped_block = markdown_block.replace("```", "")
+            return HTMLNode("code", stripped_block)
+        case MarkdownTypes.QUOTE:
+            stripped_block = markdown_block.replace(">", "")
+            return HTMLNode("blockquote", stripped_block)
+        case MarkdownTypes.ORDERED_LIST:
+            list_items = markdown_block.split("\n")
+            children_list = []
+            for item in list_items:
+                stripped_item = sub(r"^\d+.", "", item)
+                children_list.append(LeafNode("li", stripped_item))
+            return HTMLNode("ol", children=children_list)
+        case MarkdownTypes.UNORDERED_LIST:
+            list_items = markdown_block.split("\n")
+            children_list = []
+            for item in list_items:
+                stripped_item = item.replace("* ", "").replace("- ", "")
+                children_list.append(LeafNode("li", stripped_item))
+            return HTMLNode("ul", children=children_list)
+
+
+def markdown_to_html_node(markdown):
+    children = []
+    markdown_blocks = markdown_to_blocks(markdown)
+    for block in markdown_blocks:
+        children.append(markdown_block_to_html(block))
+    return HTMLNode("div", children=children)
--- src/tests/test_blocks.py
+++ src/tests/test_blocks.py
@@ -1,10 +1,45 @@
 from unittest import TestCase
-from src.blocks import markdown_to_blocks
+from src.blocks import MarkdownTypes,  markdown_to_blocks, block_to_block_type, markdown_to_html_node
+
+final_content = """
+# Tolkien Fan Club
+
+**I like Tolkien**. Read my [first post here](/majesty) (sorry the link doesn't work yet)
+
+> All that is gold does not glitter
+
+## Reasons I like Tolkien
+
+* You can spend years studying the legendarium and still not understand its depths
+* It can be enjoyed by children and adults alike
+* Disney *didn't ruin it*
+* It created an entirely new genre of fantasy
+
+## My favorite characters (in order)
+
+1. Gandalf
+2. Bilbo
+3. Sam
+4. Glorfindel
+5. Galadriel
+6. Elrond
+7. Thorin
+8. Sauron
+9. Aragorn
+
+Here's what `elflang` looks like (the perfect coding language):
+
+```
+func main(){
+    fmt.Println("Hello, World!")
+}
+```
+"""
 
 
 class TestBlocks(TestCase):
     def test_markdown_to_blocks(self):
-        markdown_to_blocks("""# This is a heading
+        result = markdown_to_blocks("""# This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
 
@@ -12,3 +47,23 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 * This is a list item
 * This is another list item""")
 
+        self.assertListEqual(result,
+                             ['# This is a heading', 'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', '* This is the first list item in a list block\n* This is a list item\n* This is another list item'])
+
+    def test_block_to_block_type(self):
+        self.assertEqual(MarkdownTypes.PARAGRAPH, block_to_block_type(
+            " This is a paragraph of text. It has some **bold** and *italic* words inside of it."))
+        self.assertEqual(MarkdownTypes.HEADING,
+                         block_to_block_type("# This is a heading"))
+        self.assertEqual(MarkdownTypes.CODE, block_to_block_type(
+            "```This is a heading```"))
+        self.assertEqual(MarkdownTypes.UNORDERED_LIST,
+                         block_to_block_type("- This is a heading"))
+        self.assertEqual(MarkdownTypes.ORDERED_LIST,
+                         block_to_block_type("1. This is a heading"))
+        self.assertEqual(MarkdownTypes.QUOTE,
+                         block_to_block_type("> This is a heading"))
+
+    def test_markdown_to_html_node(self):
+        result = markdown_to_html_node(final_content)
+        print(result)
